93586e41ad04279dd2958cb86a08b862656e1f00
SpencerHastings <shastings007@gmail.com>
2014-11-12 22:20:14 -0800

    Changed Character Movement

--- SeniorProject/Assets/Scripts/charactermovement.cs
+++ SeniorProject/Assets/Scripts/charactermovement.cs
@@ -14,19 +14,40 @@ public class charactermovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// Point to Mouse
-		Vector3 mousePos = Input.mousePosition;
-		mousePos.z = 0f;
-		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		//Vector3 mousePos = Input.mousePosition;
+		//mousePos.z = 0f;
+		//Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		//
+		//float angle = Mathf.Atan2(mousePos.y - objectPos.y, 
+		//                          mousePos.x - objectPos.x) * Mathf.Rad2Deg;
+		//transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
 
-		float angle = Mathf.Atan2(mousePos.y - objectPos.y, 
-		                          mousePos.x - objectPos.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
 
 		//Move Character
 		float vert = Input.GetAxis ("Vertical") * moveSpeed;
 		float hort = Input.GetAxis ("Horizontal") * moveSpeed;
 		velocity = new Vector3 (hort, vert, 0);
-		//transform.position += velocity;
+
+		if ((hort > 0) & (vert > 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 315));
+		if ((hort == 0) & (vert > 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+		if ((hort < 0) & (vert > 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+
+		if ((hort > 0) & (vert == 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+		//if ((hort == 0) & (vert == 0))
+		//	transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+		if ((hort < 0) & (vert == 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+
+		if ((hort > 0) & (vert < 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 215));
+		if ((hort == 0) & (vert < 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+		if ((hort < 0) & (vert < 0))
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 135));
 		
 	}
 
