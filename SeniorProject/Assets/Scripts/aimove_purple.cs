4dc52a005843396e2562da2ece6d383ccf3dfbfd
SpencerHastings <shastings007@gmail.com>
2014-12-05 10:00:24 -0800

    Added Purple Enemy

---
+++ SeniorProject/Assets/Scripts/aimove_purple.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aimove_purple : MonoBehaviour {
+
+	private GameObject Player;
+	private Animator ani;
+	private float dist;
+	private float maxrange = .8f;
+	public GameObject bullet;
+	private float cooldown = 1f;
+	private float timing;
+	private int numofbullets = 4;
+	private float bulletrefresh = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		ani = this.GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		dist = Vector3.Distance (Player.transform.position, transform.position);
+		
+		if (dist < maxrange)
+		{
+			if (timing == 0f & numofbullets > 0)
+			{
+				Shoot ();
+				timing = cooldown;
+			}
+		}
+
+		if (timing > 0f)
+		{
+			timing -= Time.deltaTime;
+		}
+
+		if (timing < 0f)
+		{
+			timing = 0f;
+		}
+	
+	}
+
+	void Shoot()
+	{
+		GameObject newbullet = (GameObject)Instantiate(bullet, transform.position, new Quaternion(0,0,0,0));
+		numofbullets -= 1;
+	}
+}
---
+++ SeniorProject/Assets/Scripts/aimove_purple_bullet.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aimove_purple_bullet : MonoBehaviour {
+
+	private GameObject Player;
+	private float speed = 20f;
+	private Vector3 velocity;
+	private float lifetime = 8f;
+	private float life = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir = Player.transform.position - transform.position;
+		dir.z = 0;
+		dir.Normalize();
+		velocity = dir * speed;
+
+		life += Time.deltaTime;
+
+		if (life >= lifetime)
+		{
+			Destroy (gameObject);
+		}
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Destroy (gameObject);
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = velocity * Time.deltaTime;
+	}
+}
