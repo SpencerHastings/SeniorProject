4efb5bafc00db6cb64862d8d3edcf3aba543d105
SpencerHastings <shastings007@gmail.com>
2014-12-11 10:01:06 -0800

    Started UI

---
+++ SeniorProject/Assets/Scripts/player_health.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class player_health : MonoBehaviour {
+
+	public float health = 100f;
+	public Slider slider;  
+	private float oldhealth;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		
+	}
+
+	void Damage(float damage)
+	{
+
+	}
+}
