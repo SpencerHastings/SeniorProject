374b82cf0c65bfa0597ab75a1ea14117eb520bfc
SpencerHastings <shastings007@gmail.com>
2015-03-09 09:58:26 -0700

    Added Doors

---
+++ SeniorProject/Assets/Scripts/GameData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel(int scene, int entrance)
+	{
+		Application.LoadLevel ("scene_03");
+	}
+}
---
+++ SeniorProject/Assets/Scripts/door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour 
+{
+	public door partner;
+	public bool isDoorFacingUp;	
+
+	public void OnTriggerEnter2D(Collider2D triggering)
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		if (partner.isDoorFacingUp)
+		{
+			player.transform.position = partner.transform.position + new Vector3(0f,.1f,0f);
+		}
+		if (!partner.isDoorFacingUp)
+		{
+			player.transform.position = partner.transform.position - new Vector3(0f,.1f,0f);
+		}
+
+	}
+
+}
